8447e17264a86d191c9be3041466ac2ac88c03e6
Julius Streit <julius.streit@student.hpi.de>
2020-01-21 14:16:40 +0100

    Adds pqp caching back

--- src/lib/sql/sql_pipeline_statement.cpp
+++ src/lib/sql/sql_pipeline_statement.cpp
@@ -185,7 +185,7 @@ const std::shared_ptr<AbstractOperator>& SQLPipelineStatement::get_physical_plan
   auto done = started;  // dummy value needed for initialization
 
   // Try to retrieve the PQP from cache
-  /*if (pqp_cache) {
+  if (pqp_cache) {
     if (const auto cached_physical_plan = pqp_cache->try_get(_sql_string)) {
       if ((*cached_physical_plan)->transaction_context_is_set()) {
         Assert(_use_mvcc == UseMvcc::Yes, "Trying to use MVCC cached query without a transaction context.");
@@ -196,7 +196,7 @@ const std::shared_ptr<AbstractOperator>& SQLPipelineStatement::get_physical_plan
       _physical_plan = (*cached_physical_plan)->deep_copy();
       _metrics->query_plan_cache_hit = true;
     }
-  }*/
+  }
 
   if (!_physical_plan) {
     // "Normal" path in which the query plan is created instead of begin retrieved from cache
