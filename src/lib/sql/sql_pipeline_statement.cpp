b857e37b35c7052d42bf27ad69418bdfc382621a
Leonard Pabst <leopab@gmx.de>
2020-03-24 12:32:42 +0100

    ignore second alias

--- src/lib/sql/sql_pipeline_statement.cpp
+++ src/lib/sql/sql_pipeline_statement.cpp
@@ -117,7 +117,8 @@ const std::shared_ptr<AbstractLQPNode>& SQLPipelineStatement::get_split_unoptimi
     bool visited_one_alias = false;
     visit_lqp(unoptimized_lqp, [&values, &parameter_id, visited_one_alias](const auto& node) mutable {
       // first alias node is ok, second is not
-        if (node && (node->type != opossum::LQPNodeType::Alias || (node->type == opossum::LQPNodeType::Alias && !visited_one_alias))) {
+        if (node) {
+          if (visited_one_alias) { break; };
           if (node->type == opossum::LQPNodeType::Alias) { visited_one_alias = true; };
             for (auto& root_expression : node->node_expressions) {
                 visit_expression(root_expression, [&values, &parameter_id](auto& expression) {
