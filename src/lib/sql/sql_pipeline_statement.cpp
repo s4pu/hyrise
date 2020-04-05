bd91b0a3747dd955664d2c97397941f8e53fa919
carla.terboven <carla.terboven@student.hpi.de>
2020-04-05 13:04:20 +0200

    debug

--- src/lib/sql/sql_pipeline_statement.cpp
+++ src/lib/sql/sql_pipeline_statement.cpp
@@ -276,7 +276,7 @@ const std::shared_ptr<AbstractLQPNode>& SQLPipelineStatement::get_optimized_logi
 
   //const auto started_preoptimization_cache = std::chrono::high_resolution_clock::now();
 
-  const auto unoptimized_lqp2 = get_unoptimized_logical_plan();
+  auto unoptimized_lqp2 = get_unoptimized_logical_plan();
   //const auto ulqp2 = unoptimized_lqp2->deep_copy();
   std::cout << "Test0" << std::endl;
 
